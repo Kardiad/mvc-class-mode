970f8901480ee156c76f0659c760536d5af2306e
Jafet Nuñez <jnunez@embount.local>
2022-07-14 10:05:17 +0200

    mejorada la estabilidad de las rutas, y la automatización de estas

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
 
 //definicion de constantes globales que sólo sirven para app... quizá las borre, pero meh
-define('base_url', 'http://localhost/tienda/');
-define('url_route', 'http://localhost/tienda/?url=');
+$mainUrl = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].''.$_SERVER['REQUEST_URI'];
+$baseUri = substr($mainUrl, 0, strrpos($mainUrl, '?'));
+
+define('base_url', (strrpos($mainUrl, '?'))==false?$mainUrl:$baseUri);
+define('url_route', $baseUri.'?url=');
 define('dbcontent', 'mysql:dbname=tienda;host=127.0.0.1');
 define('dbuser', 'root');
 define('dbpass', '');
-
 //lanzamiento de modelo, controlador y rutas
 
 require_once('./Modelos/Cliente.php');
